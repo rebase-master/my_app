de948dd6d171d8063466db437d0e44f088917150
unknown <thisismansoorkhan@gmail.com>
2014-02-18 11:17:02 +0530

    Initial Commit

---
+++ config/initializers/database.rb
@@ -0,0 +1,1 @@
+MongoMapper.database = "mp-#{Rails.env}"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyApp::Application.config.secret_key_base = 'cb82918650dc1bcf78600ab24bfc33c408200e28f7966cc7dc54636554326b2d2a89de9dc2cba47c13e939b5d20b045329b79ce805751228c38c4889cee4c1a6'
